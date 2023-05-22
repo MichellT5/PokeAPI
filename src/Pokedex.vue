9ee799bbe22352c2bcc3ba3c6aa75ad3ba5a13fc
Michel Cisneros MacBook <edgar.31896@gmail.com>
2023-05-22 10:31:40 -0700

    temp

--- src/Pokedex.vue
+++ src/Pokedex.vue
@@ -11,13 +11,36 @@
                 <ul class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                     <PokeCard v-for="poke in results" :key="poke.name" :pokemon="poke" />
                 </ul>
-                <div class="flex justify-center gap-1 mt-2">
-                    <button
-                        class="flex items-center justify-center rounded bordered w-10 h-10 p-1"
-                        :class="[page === currentPage ? 'bg-blue-700 text-white' : 'bg-zinc-200 text-black']"
-                        @click="page != currentPage ? fetchPokemon(page) : null"
-                        v-for="page in listPages" :key="page">{{ page }}</button>
-                </div>
+                <template v-if="listPages">
+                    <div class="flex justify-center gap-1 mt-2">
+                        <button
+                            class="flex items-center justify-center rounded bordered w-10 h-10 bg-zinc-200 p-1 text-black"
+                            :class="[currentPage === 1 ? 'cursor-not-allowed' : '']"
+                            @click="currentPage > 1 ? fetchPokemon(currentPage - 1) : null">
+                            <i class="fa fa-caret-left"></i>
+                        </button>
+                        <span
+                            class="flex items-center justify-center rounded bordered w-fit h-10 bg-blue-700 p-1 px-3 text-white">
+                            {{ currentPage }} of {{ pages }}
+                        </span>
+                        <button
+                            class="flex items-center justify-center rounded bordered w-10 h-10 bg-zinc-200 p-1 text-black"
+                            :class="[currentPage === pages ? 'cursor-not-allowed' : '']"
+                            @click="currentPage < pages ? fetchPokemon(currentPage + 1) : null">
+                            <i class="fa fa-caret-right"></i>
+                        </button>
+                    </div>
+                    <div class="flex items-center justify-center gap-2 mt-2">
+                        Go to page #
+                        <input class="self-stretch w-[7ch] p-2 text-black" type="number" :min="1" :max="pages"
+                            v-model="searchPage" @keyup.enter="searchPageFunc">
+                        <button
+                            class="flex items-center justify-center rounded bordered w-10 h-10 bg-zinc-200 p-1 text-black"
+                            @click="searchPageFunc">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </div>
+                </template>
             </template>
             <span v-else>Loading...</span>
         </div>
@@ -33,6 +56,7 @@ const limitValues = [12, 24, 36]
 
 const pages = ref(0)
 const currentPage = ref(1)
+const searchPage = ref(currentPage.value)
 const limit = ref(limitValues[0])
 const fetching = ref(false)
 const results = ref([] as PokeSearchResult[])
@@ -51,9 +75,16 @@ const limitChanged = () => {
     fetchPokemon(1)
 }
 
+const searchPageFunc = async () => {
+    if (searchPage.value >= 1 && searchPage.value <= pages.value) {
+        await fetchPokemon(searchPage.value)
+        searchPage.value = currentPage.value
+    }
+}
+
 const fetchPokemon = async (page: number) => {
     fetching.value = true
-    const data = await axios.get('https://pokeapi.co/api/v2/pokemon/', {
+    const data = await axios.get('https://pokeapi.co/api/v2/pokemon-species/', {
         params: {
             limit: limit.value,
             offset: (page - 1) * limit.value
--- src/components/PokeCard.vue
+++ src/components/PokeCard.vue
@@ -2,9 +2,8 @@
     <div class="relative rounded bg-zinc-200 p-3 text-black">
         <span class="absolute bottom-1 right-2" v-if="pokemonData">#{{ pokemonData.id.toString().padStart(3, '0') }}</span>
         <div class="flex items-start gap-2">
-            <img class="flex-none rounded-full bg-white" v-if="!imageLoaded"
-                src="../assets/ditto-sprite.png" :alt="name">
-            <img class="flex-none rounded-full bg-white" v-if="pokemonData" v-show="imageLoaded"
+            <img class="flex-none rounded-full bg-white" v-if="!imageLoaded" src="../assets/ditto-sprite.png" :alt="name">
+            <img class="flex-none rounded-full bg-white p-3" v-if="pokemonData" v-show="imageLoaded"
                 :src="pokemonData.sprites.front_default" @load="imageLoaded = true" :alt="name">
             <div class="flex-1">
                 <h3 class="text-xl">{{ name }}</h3>
@@ -14,8 +13,9 @@
                 </template>
                 <div v-else-if="pokemonData?.types" class="flex flex-wrap mt-5 gap-2">
                     <span class="drop-shadow-md rounded-full w-[10ch] uppercase font-mono text-center text-white"
-                        :class="[`bg-${type.name}`]" v-for="{ type } in pokemonData.types" :key="type.name">{{ type.name
-                        }}</span>
+                        :class="[`bg-${type.name}`]" v-for="{ type } in pokemonData.types" :key="type.name">
+                        {{ type.name }}
+                    </span>
                 </div>
             </div>
         </div>
